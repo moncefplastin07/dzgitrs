798d7f12ed7b750c1b2c63e1706291797a2037da
Moncef Gaha <moncefplastin07@gmail.com>
2021-07-28 22:35:24 +0100

    add data header

--- app.ts
+++ app.ts
@@ -1,25 +1,33 @@
 import { serve } from "https://deno.land/std@0.103.0/http/server.ts";
-import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { parse } from "https://deno.land/std/flags/mod.ts";
 
-import {
-  DOMParser,
-} from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 const url = "https://commits.top/algeria.html";
 const response = await fetch(url);
-const doc = new DOMParser().parseFromString(
+const document = new DOMParser().parseFromString(
   await response.text(),
   "text/html",
 )!;
 
-const usersTable = doc.querySelector(".users-list tbody")?.querySelectorAll(
-  "tr",
-)!;
+const usersTable = document.querySelector(".users-list tbody")
+  ?.querySelectorAll(
+    "tr",
+  )!;
+
+const header = {
+  lastUpdate: document.querySelector("code")?.textContent,
+  by: {
+    name: "Moncef Gaha",
+    email: "x@moncefgaha.me",
+    site: "https://moncefgaha.me",
+  },
+};
 const usersList = ([...usersTable].map((userColmun) => {
   return {
     score: +userColmun.children[0]?.textContent.split(".")[0],
     URL: userColmun.children[1]?.querySelector("a")?.getAttribute("href"),
     name: userColmun.children[1]?.lastChild.nodeValue?.substring(
-      2,
+      1,
       userColmun.children[1].lastChild.nodeValue.length - 1,
     ),
     username: userColmun.children[1]?.querySelector("a")?.getAttribute("href")
@@ -31,12 +39,14 @@ const usersList = ([...usersTable].map((userColmun) => {
 }));
 
 const { args } = Deno;
-const DEFAULT_PORT = 8000;
 const port = parse(args).port;
-
+console.log(port);
 const s = serve({ port });
-console.log("http://localhost:8000/");
 for await (const req of s) {
-  req.respond({ body: `${JSON.stringify(usersList)}` });
+  req.respond({
+    body: `${JSON.stringify({ header, users: usersList })}`,
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+  });
 }
-
